21766f201ecd075f94ddc4a1b46bcb25d7aef5b3
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-03-30 21:50:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,36 @@
-const express = require("express");
-const cors = require("cors");
-const ytdl = require("ytdl-core");
-const app = express();
-// const fs = require("fs");
+import express from 'express';
+import ytdl from 'ytdl-core';
+import slugify from 'slugify';
+const router = express.Router();
 
-const corsOptions = {
-  origin: "https://vivekfy.netlify.app", // change this origin as your like
-  // origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Sample URL: http://localhost:4000/audio/audio?url=https://www.youtube.com/watch?v=bKDdT_nyP54  //
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-app.use(express.static("./static"));
-const port = process.env.PORT || 5000;
+router.get('/play', async (req, res) => {
+    try {
+        var url = req.query.url;
+        if (!ytdl.validateURL(url)) {
+            return res.sendStatus(400);
+        }
+        let info = await ytdl.getInfo(url);
+        console.log(info.videoDetails.title);
+        const title = slugify(info.videoDetails.title, {
+            replacement: '-',
+            remove: /[*+~.()'"!:@]/g,
+            lower: true,
+            strict: false
+        });
+        // res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+        ytdl(url, {
+            format: 'mp3',
+            filter: 'audioonly',
+            quality: 'highest'
+        }).pipe(res);
 
-app.get("/", (res) => {
-  res.render("index.html");
+    } catch (err) {
+        console.error(err);
+    }
 });
 
-app.get("/get", async (req, res) => {
-  const url = req.query.url;
-  console.log(url);
-  const info = await ytdl.getInfo(url);
-  const title = info.videoDetails.title;
-  const thumbnail = info.videoDetails.thumbnails[0].url;
-  let formats = info.formats;
-
-  const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
-  // const format = ytdl.chooseFormat(info.formats, { quality: "136" });
-  formats = formats.filter((format) => format.hasAudio === true);
-
-  res.send({ title, thumbnail, audioFormats, formats });
-});
-
-app.get("/download", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
-
-  res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
-  try {
-    ytdl(url, { itag }).pipe(res);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// app.get('*', (req, res) => {
-//   res.render('error')
-// })
-
-app.listen(port, () => {
-  console.log("Running ...");
-});
+export default router;
